1e52f695f18481c33f6bf8e140375117562653e7
Abhishek Rathore <abhishekrathore325@gmail.com>
2021-10-19 17:42:15 +0530

    Navbar Created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import {HomeOutLined, MoneyCollectedOutLined, BulbOutLined, FundOutLined, MenuOutLined } from '@ant-design/icons'; 
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'; 
 
 import icon from '../images/cryptocurrency.png';
 const Navbar = () => {
@@ -12,6 +12,20 @@ const Navbar = () => {
                 <Typography.Title level={2} className = "logo">
                     <Link to = "/">CryptoWorld</Link>
                 </Typography.Title>
+                <Menu theme = "dark">
+                    <Menu.Item icon = {<HomeOutlined />}>
+                        <Link to = "/">Menu</Link>
+                    </Menu.Item>
+                    <Menu.Item icon = {<FundOutlined />}>
+                        <Link to = "/cryptocurrencies">Cryptocurrencies</Link>
+                    </Menu.Item>
+                    <Menu.Item icon = {<MoneyCollectOutlined />}>
+                        <Link to = "/exchanges">Exchanges</Link>
+                    </Menu.Item>
+                    <Menu.Item icon = {<BulbOutlined />}>
+                        <Link to = "/news">News</Link>
+                    </Menu.Item>
+                </Menu>
             </div>
         </div>
     )
