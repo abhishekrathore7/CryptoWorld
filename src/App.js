7cd821172ee6e86791bc57110bcebab0d3a54b74
Abhishek Rathore <abhishekrathore325@gmail.com>
2021-10-30 23:19:16 +0530

    Added Logo

--- src/App.js
+++ src/App.js
@@ -32,17 +32,19 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            <div className = "footer">
-                <Typography.Title level={5} style = {{ color: 'white', textAlign: 'center' }}>
-                    CryptoWorld <br />
-                    All rights reserved
-                </Typography.Title>
-                <Space>
-                    <Link to= "/">Home</Link>
-                    <Link to= "/exchanges">Exchanges</Link>
-                    <Link to= "/news">News</Link>
-                </Space>
-            </div>
+                <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+        <Link to="/">
+         <Space>CryptoWorld app.</Space>
+        </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
             </div>
         </div>
     );
